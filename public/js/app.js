d1fefe1d07be50e7c475fd79c786782c972a4c0f
Graham Melcher <melcher@gmail.com>
2013-08-01 12:58:36 -0700

    Woops, didn't mean to comment that out

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,30 @@
 'use strict';
 
-angular.module('caretracker', []).
+angular.module('caretracker', ['caretracker.filters', 'caretracker.services', 'caretracker.directives', 'caretracker.controllers']).
   config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+    $routeProvider.
+      when('/events', {
+        templateUrl: '/partials/events/index',
+        controller: 'IndexEventsCtrl'
+      }).
+      when('/events/add', {
+        templateUrl: '/partials/events/new',
+        controller: 'AddEventCtrl'
+      }).
+      when('/events/:id', {
+        templateUrl: '/partials/events/show',
+        controller: 'ShowEventCtrl'
+      }).
+      when('/events/:id/edit', {
+        templateUrl: '/partials/events/edit',
+        controller: 'EditEventCtrl'
+      }).
+      when('/events/:id/delete', {
+        templateUrl: '/partials/events/destroy_confirm',
+        controller: 'DeleteEventCtrl'
+      }).
+      otherwise({
+        redirectTo: '/events'
+      });
+    $locationProvider.html5Mode(true);
 }]);
