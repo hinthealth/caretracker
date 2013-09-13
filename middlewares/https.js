4b11a08a6197d782304199b8efcadde6ac14b925
Graham Melcher <melcher@gmail.com>
2013-09-13 10:31:30 -0700

    Don't need logging

--- middlewares/https.js
+++ middlewares/https.js
@@ -1,10 +1,7 @@
 exports.required = function (req, res, next) {
   // see above
   res.setHeader('Strict-Transport-Security', 'max-age=8640000; includeSubDomains');
-  console.log('x-forwarded-proto is ');
-  console.log(req.headers['x-forwarded-proto']);
   if (req.headers['x-forwarded-proto'] !== 'https') {
-    console.log("redirecting...");
     return res.redirect(301, 'https://' + req.headers.host + '/');
   }
   next();
